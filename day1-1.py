c2bb0515119872384fb8b3923e851f17603485f4
expsmartin <60392445+expsmartin@users.noreply.github.com>
2021-12-02 13:45:47 -0500

    Update day1-1.py

--- day1-1.py
+++ day1-1.py
@@ -15,3 +15,31 @@ for x in data:
 
 #count - 1 to remove the fact that the first line is greater than nothing.
 print(count - 1)
+#1709 
+
+next_window = []
+previous_window = 0
+count = 0
+
+input = open("day1_input.txt", "r")
+data = input.readlines()
+for x in data:
+    current_window = next_window
+    next_window = []
+    current_window.append(int(x))
+    if len(current_window) < 3:
+        print("still building window...")
+        next_window = current_window
+    elif len(current_window) == 3:
+        if sum(current_window) > previous_window:
+            print("greater")
+            count +=1
+        previous_window = sum(current_window)
+        print(current_window)
+        next_window.append(current_window[1])
+        next_window.append(current_window[2])
+        print(next_window)
+
+#count - 1 to remove the fact that the first window is greater than nothing.
+print(count - 1)
+#1761
