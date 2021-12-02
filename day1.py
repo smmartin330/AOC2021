57c187c3c620fff60a09e9c6d04b1ef89f1c2e7e
expsmartin <60392445+expsmartin@users.noreply.github.com>
2021-12-02 13:47:31 -0500

    Update day1.py

--- day1.py
+++ day1.py
@@ -5,12 +5,8 @@ input = open("day1_input.txt", "r")
 data = input.readlines()
 for x in data:
     current_line = int(x)
-    print(f"The current value is {current_line} and the previous value is {previous_line}.")
     if current_line > previous_line:
         count += 1
-        print(f"The current value is greater than the prior value and the new count is {count}.")
-    else:
-        print(f"The current value is equal to or less than the prior value and the count remains {count}.")
     previous_line = current_line
 
 #count - 1 to remove the fact that the first line is greater than nothing.
@@ -28,17 +24,13 @@ for x in data:
     next_window = []
     current_window.append(int(x))
     if len(current_window) < 3:
-        print("still building window...")
         next_window = current_window
     elif len(current_window) == 3:
         if sum(current_window) > previous_window:
-            print("greater")
             count +=1
         previous_window = sum(current_window)
-        print(current_window)
         next_window.append(current_window[1])
         next_window.append(current_window[2])
-        print(next_window)
 
 #count - 1 to remove the fact that the first window is greater than nothing.
 print(count - 1)
