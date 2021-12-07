66b8353474b7406f9f4b907781646694404d7c5f
expsmartin <60392445+expsmartin@users.noreply.github.com>
2021-12-07 17:25:58 -0500

    Update day7p2.py

--- day7p2.py
+++ day7p2.py
@@ -1,9 +1,9 @@
-#this works with the sample data but NOT the real input???? lol math is hard
+# my dataset needed the floored mean, the sample dataset needed the rounded mean. i love math. 
 
 from statistics import *
 from math import *
 
-file = open("day7_sample.txt")
+file = open("day7_input.txt")
 data = []
 
 raw_data = file.readline().strip().split(',')
@@ -11,19 +11,28 @@ raw_data = file.readline().strip().split(',')
 for x in raw_data:
     data.append(int(x))
 
+median_pos = median(data)
 rounded_mean_pos = round(mean(data))
-
+floored_mean_pos = floor(mean(data))
+print(f"Median: {median_pos}")
+print(f"Mean: {mean(data)}")
+print(f"Rounded Mean: {rounded_mean_pos}")
+print(f"Smallest value: {min(data)}")
+print(f"largest value: {max(data)}")
 total = 0
 
 def tri(x):
     return ((x**2)+x)/2
 
 for x in data:
-    if x > rounded_mean_pos:
-        total += tri(x - rounded_mean_pos)
-    elif x < rounded_mean_pos:
-        total += tri(rounded_mean_pos - x)
-    elif x == rounded_mean_pos:
+    if x > floored_mean_pos:
+        #print(f"Moving {x - rounded_mean_pos} at cost {tri(x - rounded_mean_pos)}")
+        total += tri(x - floored_mean_pos)
+    elif x < floored_mean_pos:
+        #print(f"Moving {x - rounded_mean_pos} at cost {tri(x - rounded_mean_pos)}")
+        total += tri(floored_mean_pos - x)
+    elif x == floored_mean_pos:
+        #print(f"Moving {x - rounded_mean_pos} at cost {tri(x - rounded_mean_pos)}")
         pass
 
 print(total)
