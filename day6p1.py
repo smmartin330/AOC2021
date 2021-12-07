0dafcf82a4afb5e692b705ac13e27b3b567f65f0
expsmartin <60392445+expsmartin@users.noreply.github.com>
2021-12-07 08:54:10 -0500

    Create day6p1.py

---
+++ day6p1.py
@@ -0,0 +1,20 @@
+#A list of fish sorted by their timers. 
+fish = []
+days = 80
+input = open("day6_input.txt")
+
+data = input.readline().strip().split(',')
+for d in data:
+    fish.append(int(d))
+
+
+for day in range(0,days):
+    for f in range(0,len(fish)):
+        if fish[f] == 0:
+            fish[f] = 6
+            fish.append(8)
+        else:
+            fish[f] -= 1
+    
+
+print(len(fish))
