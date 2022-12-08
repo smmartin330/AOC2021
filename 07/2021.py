bdc9c73b2790a9d5c8a089970e9259a63f8eb1e5
Stephen Martin <stephen.martin@expedient.com>
2022-12-07 23:14:34 -0500

    day 7

--- 07/2021.py
+++ 07/2021.py
@@ -1,3 +1,31 @@
+# p1
+#this works with both the sample data and the real input
+
+from statistics import *
+
+file = open("day7_input.txt")
+data = []
+
+raw_data = file.readline().strip().split(',')
+
+for x in raw_data:
+    data.append(int(x))
+
+median_pos = median(data)
+
+total = 0
+
+for x in data:
+    if x > median_pos:
+        total += (x - median_pos)
+    elif x < median_pos:
+        total += (median_pos - x)
+    elif x == median_pos:
+        pass
+
+print(total)
+
+#p2 
 # my dataset needed the floored mean, the sample dataset needed the rounded mean. i love math. 
 
 from statistics import *
